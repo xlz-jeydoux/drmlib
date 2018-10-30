fcef63084cc3fd7f5dbea1ded682d03be7c70be9
xlz-jeydoux <jeydoux@accelize.com>
2018-10-30 10:22:04 +0100

    a

--- docs/conf.py
+++ docs/conf.py
@@ -24,24 +24,24 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    ##subprocess.call('cd ../doxygen; doxygen doxygen.cfg', shell=True)
-    #subprocess.call('pip install breathe', shell=True)
-    #subprocess.call('doxygen doxygen.cfg', shell=True)  
-    #subprocess.call('doxygen ../build/doc/Doxyfile', shell=True) 
-    
-
     #subprocess.call('cd ../doxygen; doxygen doxygen.cfg', shell=True)
     subprocess.call('pip install breathe', shell=True)
-    subprocess.call('pip install cmake', shell=True)
-    subprocess.call('env', shell=True)
-    subprocess.call('sh -c \"$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)\" ', shell=True)
-    subprocess.call('brew install libjsoncpp', shell=True)
-    subprocess.call('sudo apt-get  --assume-yes install libjsoncpp-dev', shell=True)
-    subprocess.call('sudo apt-get  --assume-yes install libcurl4-openssl-dev', shell=True)
-    subprocess.call('sudo apt-get  --assume-yes install unifdef', shell=True)
-    subprocess.call('cd .. && mkdir build && cd build && cmake -DDOC=ON .. && make && cd ..  ', shell=True)  
-    #subprocess.call('cd ../doxygen; doxygen doxygen.cfg', shell=True)  
+    subprocess.call('doxygen doxygen.cfg', shell=True)  
     subprocess.call('doxygen ../build/doc/Doxyfile', shell=True) 
+    
+
+    ##subprocess.call('cd ../doxygen; doxygen doxygen.cfg', shell=True)
+    #subprocess.call('pip install breathe', shell=True)
+    #subprocess.call('pip install cmake', shell=True)
+    #subprocess.call('env', shell=True)
+    #subprocess.call('sh -c \"$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)\" ', shell=True)
+    #subprocess.call('brew install libjsoncpp', shell=True)
+    #subprocess.call('sudo apt-get  --assume-yes install libjsoncpp-dev', shell=True)
+    #subprocess.call('sudo apt-get  --assume-yes install libcurl4-openssl-dev', shell=True)
+    #subprocess.call('sudo apt-get  --assume-yes install unifdef', shell=True)
+    #subprocess.call('cd .. && mkdir build && cd build && cmake -DDOC=ON .. && make && cd ..  ', shell=True)  
+    ##subprocess.call('cd ../doxygen; doxygen doxygen.cfg', shell=True)  
+    #subprocess.call('doxygen ../build/doc/Doxyfile', shell=True) 
     
     
     
